d073818d1255b7edeb507f104dd7eb0bf62929bd
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2021-12-10 00:24:51 -0300

    fix: mostrar ocultar nav según autenticación

--- fichas-front/src/app/app.component.ts
+++ fichas-front/src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { UsuariosService } from './services/usuarios.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [UsuariosService],
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
+  public identificado: boolean = false;
+  public usuario!: any;
 
   constructor(
     private usuariosService: UsuariosService) { }
+
+  ngDoCheck() {
+    if (this.usuariosService.loggedIn()) {
+      this.identificado = true
+    } else {
+      this.identificado = false;
+    }
+  }
 
   logOut() {
     this.usuariosService.logOut();
